77a4e0b4fe0dd387b3f8295f7b70d3ac5010ce41
AnhTus03 <89677756+AnhTus03@users.noreply.github.com>
2021-08-28 12:33:47 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'em à!',
+    introTitle: 'Changg🐷!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    title: 'Con lợn của anh ưii 😙',
+    desc: 'nếu được reset cuộc đời em vẫn chọn yêu anh chứ ? ',
+    btnYes: 'Em yêu anh <3',
+    btnNo: 'Không t ghét mày..',
+    question:'Anh biết dạo gần đây có một số chuyện làm em không vui, với lại cũng là do yêu xa 1 phần có lẽ cảm xúc không còn được như trước nữa, không cần biết là lí do gì nhưng làm em buồn làm em chán lỗi đều do anh hết..Mình cùng cố gắng vượt qua thử thách này nhé<3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    reply: 'Em yêu anh, thật sự yêu rất nhiều :((',
+    mess: 'Anh biết mà..thương changg của anh lắm.. Yêu em nhiều nhiều',
+    messDesc: 'Vẫn cứ là một tình yêu đẹp nhé.. anh sẽ cố gắng ',
+    btnAccept: 'Vâng anh..<3',
+    messLink: 'http://m.me/anhtu.t35' //m.me/anhtu.t35 VD: https://m.me/nam.nodemy
 }
